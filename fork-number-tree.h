44bc581e4ee67992fb4e98aa4a5b22551086a0dd
Joshua Weinstein <jweinst1@berkeley.edu>
2023-12-27 23:49:50 -0800

    mt support

--- fork-number-tree.h
+++ fork-number-tree.h
@@ -24,7 +24,7 @@ namespace FNTree {
 
 	static constexpr BitCovers BIT_COVERS;
 
-	constexpr size_t hashData(void* data, size_t size) {
+	inline constexpr size_t hashData(void* data, size_t size) {
 		size_t hash = 5381;
 		uint64_t* reader = (uint64_t*)data;
 		size_t count = size / sizeof(uint64_t);
@@ -50,6 +50,25 @@ namespace FNTree {
 		void printValue() const {
 			int* reader = (int*)value;
 			printf("%d\n", *reader);
+		}
+	};
+
+	struct KeyValueList {
+		KeyValuePair** lst = nullptr;
+		size_t len = 0;
+		size_t cap = 0;
+
+		KeyValueList(KeyValuePair* kvp, size_t cap): cap(cap) {
+			lst = (KeyValuePair**)calloc(cap, sizeof(KeyValuePair*));
+			lst[len++] = kvp;
+		}
+
+		void push(KeyValuePair* kvp) {
+			if (len == cap) {
+				cap *= cap;
+				lst = (KeyValuePair**)realloc(lst, cap);
+			}
+			lst[len++] = kvp;
 		}
 	};
 
@@ -108,7 +127,7 @@ namespace FNTree {
 	static size_t totalMemUsed = 0;
 	static size_t collCount = 0;
 
-	template <size_t keySize, size_t childCount>
+	template <size_t keySize, size_t childCount /*, size_t partLevel = (size_t)-1*/>
 	struct BitNode {
 		template<size_t amount, size_t bCount>
 		struct OffSetArr {
@@ -134,9 +153,24 @@ namespace FNTree {
 		static constexpr OffSetArr<keySize / bitCount, bitCount> offsets;
 		static constexpr size_t offsetsSize = sizeof(offsets.offsets) / sizeof(offsets.offsets[0]);
 		static constexpr size_t bridgesSize = offsetsSize - 1;
-		//void* data = nullptr;
+		void* lock = nullptr;
 		void* children[childCount] = {nullptr};
 	};
+
+	template <size_t keySize, size_t childCount>
+	void makeParitions(BitNode<keySize, childCount>* tree, size_t level) {
+		if (level == 0) {
+			tree->lock = new ParitionLock();
+			return;
+		}
+		for (size_t i = 0; i < childCount; ++i)
+		{
+			tree->children[i] = new BitNode<keySize, childCount>();
+			if (level > 0) {
+				makeParitions((BitNode<keySize, childCount>*)tree->children[i], level - 1);
+			}
+		}
+	}
 
 	template <size_t keySize, size_t childCount>
 	void insertInto(BitNode<keySize, childCount>* tree, size_t key, void* data) {
@@ -159,6 +193,50 @@ namespace FNTree {
 	void insertHash(BitNode<keySize, childCount>* tree, size_t key, KeyValuePair* kvp) {
 		BitNode<keySize, childCount>* current = tree;
 		size_t i = 0;
+		for (; i < BitNode<keySize, childCount>::bridgesSize; ++i)
+		{
+			//printf("key slice is %zu\n", key >> BitNode<keySize, childCount>::offsets.offsets[i]);
+			size_t shifted = (key >> BitNode<keySize, childCount>::offsets.offsets[i]) & BitNode<keySize, childCount>::bitShift;
+			//printf("slice %zu ", shifted);
+			if (current->children[shifted] == nullptr) {
+				current->children[shifted] = new BitNode<keySize, childCount>();
+			} 
+			current =  (BitNode<keySize, childCount>*)current->children[shifted];
+		}
+		size_t shiftedLast = (key >> BitNode<keySize, childCount>::offsets.offsets[i]) & BitNode<keySize, childCount>::bitShift;
+		//printf("slice %zu\n", shiftedLast);
+		KeyValueSpot* gotkv = (KeyValueSpot*)current->children[shiftedLast];
+		if (gotkv == nullptr) {
+			KeyValueSpot* newkvs = new KeyValueSpot();
+			newkvs->kvp = kvp;
+			current->children[shiftedLast] = newkvs;
+		} else {
+			FNTree::collCount += 1;
+			while (gotkv != nullptr) {
+				if (std::memcmp(gotkv->kvp->key, kvp->key, sizeof(kvp->key)) == 0) {
+					gotkv->kvp->value = kvp->value;
+					return;
+				}
+				gotkv = gotkv->next;
+			}
+			KeyValueSpot* newkvs = new KeyValueSpot();
+			newkvs->kvp = kvp;
+			gotkv = (KeyValueSpot*)current->children[shiftedLast];
+			newkvs->next = gotkv;
+			current->children[shiftedLast] = newkvs;
+		}
+	}
+
+	template <size_t keySize, size_t childCount>
+	void insertHashPart(BitNode<keySize, childCount>* tree, size_t key, KeyValuePair* kvp, size_t levels) {
+		BitNode<keySize, childCount>* current = tree;
+		size_t i = 0;
+		for (; i < levels; ++i)
+		{
+			current = (BitNode<keySize, childCount>*)current->children[(key >> BitNode<keySize, childCount>::offsets.offsets[i]) & BitNode<keySize, childCount>::bitShift];
+		}
+		// now the lock part
+		ScopedPartLock scoped((ParitionLock*)current->lock);
 		for (; i < BitNode<keySize, childCount>::bridgesSize; ++i)
 		{
 			//printf("key slice is %zu\n", key >> BitNode<keySize, childCount>::offsets.offsets[i]);
@@ -235,6 +313,36 @@ namespace FNTree {
 	}
 
 	template <size_t keySize, size_t childCount>
+	void* findHashPart(BitNode<keySize, childCount>* tree, size_t key, KeyValuePair* kvp, size_t levels) {
+		BitNode<keySize, childCount>* current = tree;
+		//printf("FIND offsetsize is %zu\n", BitNode<keySize, childCount>::offsetsSize);
+		size_t i = 0;
+		for (; i < levels; ++i)
+		{
+			current = (BitNode<keySize, childCount>*)current->children[(key >> BitNode<keySize, childCount>::offsets.offsets[i]) & BitNode<keySize, childCount>::bitShift];
+		}
+		// now the lock part
+		ScopedPartLock scoped((ParitionLock*)current->lock);
+		for (; i < BitNode<keySize, childCount>::bridgesSize; ++i)
+		{
+			size_t shifted = (key >> BitNode<keySize, childCount>::offsets.offsets[i]) & BitNode<keySize, childCount>::bitShift;
+			if (current->children[shifted] == nullptr) {
+				return nullptr;
+			}
+			current = (BitNode<keySize, childCount>*)current->children[shifted];
+		}
+		size_t shiftedLast = (key >> BitNode<keySize, childCount>::offsets.offsets[i]) & BitNode<keySize, childCount>::bitShift;
+		KeyValueSpot* gotkv = (KeyValueSpot*)current->children[shiftedLast];
+		while (gotkv != nullptr) {
+			if (std::memcmp(gotkv->kvp->key, kvp->key, sizeof(kvp->key)) == 0) {
+				return gotkv->kvp->value;
+			}
+			gotkv = gotkv->next;
+		}
+		return nullptr;
+	}
+
+	template <size_t keySize, size_t childCount>
 	void* removeInto(BitNode<keySize, childCount>* tree, size_t key) {
 		BitNode<keySize, childCount>* current = tree;
 		size_t i = 0;
@@ -257,6 +365,10 @@ namespace FNTree {
 		static constexpr size_t mapChildCount = 32;
 		BitNode<mapKeySize, mapChildCount> _bnode;
 
+		MapObj() {
+			makeParitions<mapKeySize, mapChildCount>(&_bnode, 3);
+		}
+
 		void insert(KeyValuePair* kvp) {
 			size_t hash_key = hashData(kvp->key, sizeof(kvp->key));
 			//printf("got the hash %zu, bit cover %zu\n", hash_key & BIT_COVERS.shifts[mapKeySize], BIT_COVERS.shifts[mapKeySize]);
@@ -265,12 +377,12 @@ namespace FNTree {
 			//	printf("%u ", kvp->key[i]);
 			//}
 			//printf("\n");
-			insertHash<mapKeySize, mapChildCount>(&_bnode, hash_key, kvp);
+			insertHashPart<mapKeySize, mapChildCount>(&_bnode, hash_key, kvp, 3);
 		}
 
 		void* find(KeyValuePair* kvp) {
 			size_t hash_key = hashData(kvp->key, sizeof(kvp->key));
-			return findHash<mapKeySize, mapChildCount>(&_bnode, hash_key, kvp);
+			return findHashPart<mapKeySize, mapChildCount>(&_bnode, hash_key, kvp, 3);
 		}
 	};
 }
---
+++ tester.cpp
@@ -0,0 +1,210 @@
+#include "fork-number-tree.h"
+
+#include <chrono>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <functional>
+#include <vector>
+#include <map>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+#include <random>
+#include <thread>
+#include <atomic>
+
+using namespace std::chrono;
+
+static std::vector<FNTree::KeyValuePair*> NUM_BANK;
+static std::vector<std::string> STR_BANK;
+
+#define BANK_TEST_SIZE 1000000
+
+static void populateBank() {
+	for (size_t i = 0; i < BANK_TEST_SIZE; ++i)
+	{
+		FNTree::KeyValuePair* newkvs = new FNTree::KeyValuePair();
+		for (size_t i = 0; i < sizeof(newkvs->key); ++i)
+		{
+			newkvs->key[i] = (unsigned char)rand();
+			newkvs->value = new int(rand());
+		}
+		NUM_BANK.push_back(newkvs);
+	}
+}
+
+static void populateStrBank() {
+	for (size_t i = 0; i < BANK_TEST_SIZE; ++i)
+	{
+		std::string foo;
+		for (size_t i = 0; i < 16; ++i)
+		{
+			foo.push_back((char)rand());
+		}
+		STR_BANK.push_back(foo);
+	}
+
+}
+
+static FNTree::MapObj aNode;
+static std::unordered_map<std::string, void*> aMap;
+
+void tester_map_func(void) {
+	for (std::vector<std::string>::iterator i = STR_BANK.begin(); i != STR_BANK.end(); ++i)
+	{
+		aMap[*i] = (void*)i->c_str();
+	}
+}
+
+void lookup_map_func(void) {
+	size_t adder = 0;
+	for (std::vector<std::string>::iterator i = STR_BANK.begin(); i != STR_BANK.end(); ++i)
+	{
+		void* found = aMap.find(*i)->second;
+		adder += (size_t)found;
+
+	}
+	printf("adder %zu\n", adder);
+}
+
+
+void tester_func(void) {
+	for (std::vector<FNTree::KeyValuePair*>::iterator i = NUM_BANK.begin(); i != NUM_BANK.end(); ++i)
+	{
+		//printf("INSERTING KEY ");
+		//(*i)->printKey();
+		//printf("TO VALUE ");
+		//(*i)->printValue();
+		//FNTree::insertInto(&aNode, *i, (void*)*i);
+		aNode.insert(*i);
+	}
+}
+
+void tester_func_spec(const std::vector<FNTree::KeyValuePair*>& numbers) {
+	for (auto i = numbers.begin(); i != numbers.end(); ++i)
+	{
+		//printf("INSERTING KEY ");
+		//(*i)->printKey();
+		//printf("TO VALUE ");
+		//(*i)->printValue();
+		//FNTree::insertInto(&aNode, *i, (void*)*i);
+		aNode.insert(*i);
+	}
+}
+
+void deleter_func(void) {
+	for (std::vector<FNTree::KeyValuePair*>::iterator i = NUM_BANK.begin(); i != NUM_BANK.end(); ++i)
+	{
+		//size_t result = removeFull(&aNode, *i);
+		//printf("removed %zu nodes\n", result);
+	}
+}
+
+void lookup_func(void) {
+	size_t adder = 0;
+	for (std::vector<FNTree::KeyValuePair*>::iterator i = NUM_BANK.begin(); i != NUM_BANK.end(); ++i)
+	{
+		void* found = aNode.find(*i);
+		//printf("LOOK UP KEY ");
+		//(*i)->printKey();
+		//printf("TO VALUE %d\n", *(int*)(found));
+		adder += (size_t)found;
+	}
+	printf("adder %zu\n", adder);
+}
+
+void lookup_func_spec(const std::vector<FNTree::KeyValuePair*>& numbers) {
+	size_t adder = 0;
+	for (auto i = numbers.begin(); i != numbers.end(); ++i)
+	{
+		void* found = aNode.find(*i);
+		//printf("LOOK UP KEY ");
+		//(*i)->printKey();
+		//printf("TO VALUE %d\n", *(int*)(found));
+		adder += (size_t)found;
+	}
+	printf("adder %zu\n", adder);
+}
+
+void time_function(const char* name, std::function<void(void)> func, unsigned times) {
+	auto start = high_resolution_clock::now();
+	for (unsigned i = 0; i < times; ++i)
+	{
+		func();
+	}
+	auto stop = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop - start);
+	std::printf("%s -> US %llu\n", name, duration.count());
+}
+
+void mt_tester(void) {
+	static constexpr size_t threadCount = 8;
+	std::thread tpool[threadCount];
+	static std::atomic<bool> waitForStart = true;
+
+	for (size_t i = 0; i < threadCount; ++i)
+	{
+		tpool[i] = std::thread([&]{
+			std::vector<FNTree::KeyValuePair*> myCopy = NUM_BANK;
+			std::random_device rd;
+			std::mt19937 g(rd());
+			std::shuffle(myCopy.begin(), myCopy.end(), g);
+			while (waitForStart.load()) {
+
+			}
+			auto start = high_resolution_clock::now();
+			lookup_func_spec(myCopy);
+			auto stop = high_resolution_clock::now();
+			auto duration = duration_cast<microseconds>(stop - start);
+			std::printf("%s -> US %llu\n", "threaded lookup", duration.count());
+
+		});
+	}
+	waitForStart.store(false);
+
+	for (size_t j = 0; j < threadCount; ++j)
+	{
+		tpool[j].join();
+	}
+}
+
+/*
+PERF BOOST -O3
+18 bit 64 children
+insert bit test -> US 2941
+mem used 2130432
+adder 1073064323333195
+ilookup bit test -> US 1791
+insert map test -> US 148122
+adder 1073806376451147
+ilookup map test -> US 19345
+*/
+
+
+static void perfTesting() {
+	populateBank();
+	populateStrBank();
+	time_function("insert bit test", tester_func, 1);
+	printf("mem used %zu\n", FNTree::totalMemUsed);
+	printf("coll used %zu\n", FNTree::collCount);
+	time_function("ilookup bit test", lookup_func, 1);
+	//deleter_func();
+
+	time_function("insert map test", tester_map_func, 1);
+	time_function("ilookup map test", lookup_map_func, 1);
+
+	mt_tester();
+}
+
+int main(int argc, char const *argv[])
+{
+	perfTesting();
+	
+	return 0;
+}
